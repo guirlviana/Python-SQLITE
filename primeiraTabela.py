72814b4db297e8fa0ded12ecc7bc8f42442aeca7
Guilherme <guirlviana@gmail.com>
2020-07-13 22:08:53 -0300

    Adicionado novo registro

--- primeiraTabela.py
+++ primeiraTabela.py
@@ -1,7 +1,7 @@
 import sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 class Artista(Base):
@@ -11,3 +11,13 @@ class Artista(Base):
 
 engine = create_engine('sqlite:///artistas.db', echo=True)
 Base.metadata.create_all(bind=engine)
+
+Conexao = sessionmaker(bind=engine)
+conexao = Conexao()
+conexao: sessionmaker
+
+novo_artista = Artista()
+novo_artista.artista_id = 1
+novo_artista.nome = "Artista 1"
+conexao.add(novo_artista)
+conexao.commit()
