2f12a198a8c21b4c44113219d6b52ea01af6f5c5
Guilherme <guirlviana@gmail.com>
2020-07-13 22:30:23 -0300

    Album Class

--- primeiraTabela.py
+++ primeiraTabela.py
@@ -1,13 +1,22 @@
 import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Sequence, Numeric, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 Base = declarative_base()
 
 class Artista(Base):
     __tablename__ = "artista"
-    artista_id = Column(Integer, primary_key=True)
+    artista_id = Column(Integer,Sequence("artista_id_auto_incremento", start=1), primary_key=True)
     nome = Column(String)
+    albuns = relationship('Album')
+
+class Album(Base):
+    __tablename__ = "album"
+    album_id = Column(Integer, Sequence("album_id_auto_incremento", start=1), primary_key=True) 
+    titulo = Column(String)
+    preco = Column(Numeric(10,2))
+    artista_id = Column(Integer, ForeignKey('artista.artista_id'))
+    
 
 engine = create_engine('sqlite:///artistas.db', echo=True)
 Base.metadata.create_all(bind=engine)
@@ -22,14 +31,14 @@ conexao: sessionmaker
 # conexao.add(novo_artista)
 # conexao.commit()
 
-resultado = conexao.query(Artista).all()
+# resultado = conexao.query(Artista).all()
 
-for item in resultado:
-    print(item.artista_id)
-    print(item.nome)
+# for item in resultado:
+#     print(item.artista_id)
+#     print(item.nome)
 
-novo_artista2 = Artista()
-novo_artista2.artista_id = 2
-novo_artista2.nome = "Artista 2"
-conexao.add(novo_artista2)
-conexao.commit()
+# novo_artista2 = Artista()
+# novo_artista2.artista_id = 2
+# novo_artista2.nome = "Artista 2"
+# conexao.add(novo_artista2)
+# conexao.commit()
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-novo_artista = Artista()
-# novo_artista.artista_id = 1
-# novo_artista.nome = "Artista 1"
